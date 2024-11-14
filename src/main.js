1ef271c339bd6b37b2989583e37e98f1f60c4d09
Anatoliy Daniluyk <anatolydanilyuk2002@gmail.com>
2024-11-14 15:25:23 +0200

    task

---
+++ src/main.js
@@ -0,0 +1,569 @@
+const customerData = [
+  {
+    customername: 'Jane Cooper',
+    company: 'Microsoft',
+    phonenumber: '555-0118',
+    email: 'jane@microsoft.com',
+    country: 'United States',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Ronald Richards',
+    company: 'Adobe',
+    phonenumber: '555-0107',
+    email: 'ronald@adobe.com',
+    country: 'Israel',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Marvin McKinney',
+    company: 'Tesla',
+    phonenumber: '555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
+    status: 'Active',
+  },
+  {
+    customername: 'Jerome Bell',
+    company: 'Google',
+    phonenumber: '555-0129',
+    email: 'jerome@google.com',
+    country: 'Réunion',
+    status: 'Active',
+  },
+  {
+    customername: 'Kathryn Murphy',
+    company: 'Microsoft',
+    phonenumber: '555-0120',
+    email: 'kathryn@microsoft.com',
+    country: 'Curaçao',
+    status: 'Active',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Ronald Richards',
+    company: 'Adobe',
+    phonenumber: '555-0107',
+    email: 'ronald@adobe.com',
+    country: 'Israel',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Marvin McKinney',
+    company: 'Tesla',
+    phonenumber: '555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
+    status: 'Active',
+  },
+  {
+    customername: 'Jane Cooper',
+    company: 'Microsoft',
+    phonenumber: '555-0118',
+    email: 'jane@microsoft.com',
+    country: 'United States',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Ronald Richards',
+    company: 'Adobe',
+    phonenumber: '555-0107',
+    email: 'ronald@adobe.com',
+    country: 'Israel',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Marvin McKinney',
+    company: 'Tesla',
+    phonenumber: '555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
+    status: 'Active',
+  },
+  {
+    customername: 'Jane Cooper',
+    company: 'Microsoft',
+    phonenumber: '555-0118',
+    email: 'jane@microsoft.com',
+    country: 'United States',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jane Cooper',
+    company: 'Microsoft',
+    phonenumber: '555-0118',
+    email: 'jane@microsoft.com',
+    country: 'United States',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Ronald Richards',
+    company: 'Adobe',
+    phonenumber: '555-0107',
+    email: 'ronald@adobe.com',
+    country: 'Israel',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Marvin McKinney',
+    company: 'Tesla',
+    phonenumber: '555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
+    status: 'Active',
+  },
+  {
+    customername: 'Jerome Bell',
+    company: 'Google',
+    phonenumber: '555-0129',
+    email: 'jerome@google.com',
+    country: 'Réunion',
+    status: 'Active',
+  },
+  {
+    customername: 'Kathryn Murphy',
+    company: 'Microsoft',
+    phonenumber: '555-0120',
+    email: 'kathryn@microsoft.com',
+    country: 'Curaçao',
+    status: 'Active',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Ronald Richards',
+    company: 'Adobe',
+    phonenumber: '555-0107',
+    email: 'ronald@adobe.com',
+    country: 'Israel',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Marvin McKinney',
+    company: 'Tesla',
+    phonenumber: '555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
+    status: 'Active',
+  },
+  {
+    customername: 'Jane Cooper',
+    company: 'Microsoft',
+    phonenumber: '555-0118',
+    email: 'jane@microsoft.com',
+    country: 'United States',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Jacob Jones',
+    company: 'Yahoo',
+    phonenumber: '555-0112',
+    email: 'jacob@yahoo.com',
+    country: 'Brazil',
+    status: 'Active',
+  },
+  {
+    customername: 'Kristin Watson',
+    company: 'Facebook',
+    phonenumber: '555-0127',
+    email: 'kristin@facebook.com',
+    country: 'Åland Islands',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Ronald Richards',
+    company: 'Adobe',
+    phonenumber: '555-0107',
+    email: 'ronald@adobe.com',
+    country: 'Israel',
+    status: 'Inactive',
+  },
+  {
+    customername: 'Marvin McKinney',
+    company: 'Tesla',
+    phonenumber: '555-0126',
+    email: 'marvin@tesla.com',
+    country: 'Iran',
+    status: 'Active',
+  },
+  {
+    customername: 'Jane Cooper',
+    company: 'Microsoft',
+    phonenumber: '555-0118',
+    email: 'jane@microsoft.com',
+    country: 'United States',
+    status: 'Active',
+  },
+  {
+    customername: 'Floyd Miles',
+    company: 'Yahoo',
+    phonenumber: '555-0100',
+    email: 'floyd@yahoo.com',
+    country: 'Kiribati',
+    status: 'Inactive',
+  },
+];
+
+const customerBody = document.querySelector('.customer__body');
+const paginationContainer = document.querySelector('.pagination__buttons');
+
+const prevButton = document.querySelector('.pagination__button--prev');
+const nextButton = document.querySelector('.pagination__button--next');
+
+const searchInput = document.querySelector('.customer__search');
+
+const totalItems = customerData.length;
+const itemsPerPage = 8;
+let curentPage = 1;
+let filteredData = customerData;
+const totalPages = calculateTotalPages(filteredData.length, itemsPerPage);
+
+setPage(1);
+
+prevButton.addEventListener('click', () => {
+  if (curentPage > 1) {
+    const page = curentPage - 1;
+    setPage(page);
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (curentPage < totalPages) {
+    const page = curentPage + 1;
+    setPage(page);
+  }
+});
+
+searchInput.addEventListener('input', event => {
+  const query = event.target.value.toLowerCase();
+  filteredData = customerData.filter(customer =>
+    customer.customername.toLowerCase().includes(query)
+  );
+  const updatedTotalPages = calculateTotalPages(
+    filteredData.length,
+    itemsPerPage
+  );
+  updateGrid(1, itemsPerPage);
+  updatePagination(1, updatedTotalPages);
+});
+
+function generateTable(data) {
+  const headers = [
+    'Customer Name',
+    'Company',
+    'Phone Number',
+    'Email',
+    'Country',
+    'Status',
+  ];
+  const table = document.createElement('div');
+  table.classList.add('customer__table');
+
+  headers.forEach(header => {
+    const headerCell = document.createElement('div');
+    headerCell.classList.add('customer__table-column');
+    headerCell.textContent = header;
+    table.appendChild(headerCell);
+  });
+
+  data.forEach(row => {
+    headers.forEach(header => {
+      const cell = document.createElement('div');
+      cell.classList.add('customer__table-cell');
+
+      if (header === 'Status') {
+        cell.classList.add(
+          row.status === 'Active'
+            ? 'customer__table-cell--status-active'
+            : 'customer__table-cell--status-inactive'
+        );
+      }
+
+      cell.textContent = row[header.toLowerCase().replace(' ', '')];
+      table.appendChild(cell);
+    });
+  });
+
+  customerBody.innerHTML = '';
+  customerBody.appendChild(table);
+}
+
+function calculateTotalPages(totalItems, itemsPerPage) {
+  return Math.ceil(totalItems / itemsPerPage);
+}
+
+function updateGrid(page, pageSize) {
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedData = filteredData.slice(startIndex, endIndex);
+  generateTable(paginatedData);
+}
+
+function updatePagination(curentPage, totalPages) {
+  paginationContainer.innerHTML = '';
+
+  const createPageButton = pageNumber => {
+    return `<button class="pagination__button ${
+      curentPage === pageNumber ? 'current' : ''
+    }">${pageNumber}</button>`;
+  };
+
+  if (curentPage < 4) {
+    if (totalPages > 5) {
+      paginationContainer.innerHTML += createPageButton(1);
+      paginationContainer.innerHTML += createPageButton(2);
+      paginationContainer.innerHTML += createPageButton(3);
+      paginationContainer.innerHTML += createPageButton(4);
+      paginationContainer.innerHTML +=
+        '<span class="pagination__ellipsis">...</span>';
+      paginationContainer.innerHTML += createPageButton(totalPages);
+    } else {
+      for (let i = 1; i <= totalPages; i++) {
+        paginationContainer.innerHTML += createPageButton(i);
+      }
+    }
+  } else {
+    if (totalPages - curentPage < 3) {
+      paginationContainer.innerHTML += createPageButton(1);
+      paginationContainer.innerHTML +=
+        '<span class="pagination__ellipsis">...</span>';
+      for (let i = Math.max(totalPages - 4, 2) + 1; i <= totalPages; i++) {
+        paginationContainer.innerHTML += createPageButton(i);
+      }
+    } else {
+      if (totalPages > 5) {
+        paginationContainer.innerHTML += createPageButton(1);
+        paginationContainer.innerHTML +=
+          '<span class="pagination__ellipsis">...</span>';
+        paginationContainer.innerHTML += createPageButton(curentPage - 1);
+        paginationContainer.innerHTML += `<button class="pagination__button current">${curentPage}</button>`;
+        paginationContainer.innerHTML += createPageButton(curentPage + 1);
+        paginationContainer.innerHTML +=
+          '<span class="pagination__ellipsis">...</span>';
+        paginationContainer.innerHTML += createPageButton(totalPages);
+      } else {
+        for (let i = 1; i <= totalPages; i++) {
+          paginationContainer.innerHTML += createPageButton(i);
+        }
+      }
+    }
+  }
+
+  const buttons = document.querySelectorAll('.pagination__button');
+  buttons.forEach(button => {
+    button.addEventListener('click', event => {
+      const pageNumber = parseInt(event.target.textContent);
+      if (!isNaN(pageNumber)) {
+        setPage(pageNumber);
+      }
+    });
+  });
+}
+
+function setPage(page) {
+  curentPage = page;
+  updateGrid(page, itemsPerPage);
+  updatePagination(page, totalPages);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerButton = document.querySelector('.burger-menu__button');
+  const sidebarMenu = document.querySelector('.sidebar-menu');
+
+  function checkScreenWidth() {
+    if (window.innerWidth > 768) {
+      sidebarMenu.style.display = 'flex';
+    } else {
+      sidebarMenu.style.display = 'none';
+    }
+  }
+
+  checkScreenWidth();
+  burgerButton.addEventListener('click', function () {
+    if (
+      sidebarMenu.style.display === 'none' ||
+      sidebarMenu.style.display === ''
+    ) {
+      sidebarMenu.style.display = 'flex';
+    } else {
+      sidebarMenu.style.display = 'none';
+    }
+  });
+
+  window.addEventListener('resize', function () {
+    checkScreenWidth();
+  });
+});
